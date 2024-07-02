59c27ac6cc6fc227f8bc2c5429fc6b75deae0a60
Eduardo Sevilha <166170981+EduardoSevilha@users.noreply.github.com>
2024-07-02 19:35:41 -0300

    Finalização

---
+++ Aula12ex/Ex14/script.js
@@ -0,0 +1,21 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        img.src = 'imagens/manha.png'
+        msg.innerText = 'Bom Dia!'
+        document.body.style.background = '#FFE964'
+    }else if(hora >= 12 && hora <= 18){
+        img.src = 'imagens/tarde.png'
+        msg.innerText = 'Boa Tarde!'
+        document.body.style.background = '#C8DCE6'
+    }else{
+        img.src = 'imagens/noite.png'
+        msg.innerText = 'Boa Noite!'
+        document.body.style.background = '#02306A'
+    }
+}
+
