c23b411b7aef28076d78285cc14141fdcab75558
Eduardo Sevilha <166170981+EduardoSevilha@users.noreply.github.com>
2024-07-04 16:44:17 -0300

    testes

--- Aula12ex/Ex14/script.js
+++ Aula12ex/Ex14/script.js
@@ -2,10 +2,11 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 8
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12){
-        img.src = 'imagens/manha.png'
+        img.src = 'imagens/manha11.png'
         msg.innerText = 'Bom Dia!'
         document.body.style.background = '#FFE964'
     }else if(hora >= 12 && hora <= 18){
